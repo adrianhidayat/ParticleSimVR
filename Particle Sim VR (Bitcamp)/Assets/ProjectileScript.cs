83b9f5eefba1d5b2f693486f01537ce0231f752c
PlasmaFlare <richardli137@gmail.com>
2017-04-08 14:29:14 -0400

    added some projectiles (not moving)

---
+++ Particle Sim VR (Bitcamp)/Assets/ProjectileScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour {
+
+    private Rigidbody rigidbody;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+
+        var angle = rigidbody.rotation;
+        var r = speed;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Particle Sim VR (Bitcamp)/Assets/Scripts/ShootScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ShootScript : MonoBehaviour {
+
+    public GameObject projectile;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var joystickNames = Input.GetJoystickNames();
+
+        if (joystickNames.Contains("Oculus Remote") && Input.GetAxisRaw("Fire") == 1) {
+            // Shoot stuff
+            Instantiate(projectile, gameObject.transform);
+        }
+	}
+}
